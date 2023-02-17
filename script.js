341892bcbeb691e23914cd26170926a31af65957
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-02-17 12:14:04 +0100

    etape 4

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
-function askNumber() {
-  let givenNumber;
-  console.log("fonction asknumber");
+// function askNumber() {
+//   let givenNumber;
+//   console.log("fonction asknumber");
+//   do {
+//     givenNumber = parseInt(prompt("Entrez un nombre"));
+//   } while (isNaN(givenNumber));
+//   return givenNumber;
+// }
+
+// function didIWin(givenNumber) {
+//   console.log("fonction didwin");
+
+//   console.log("tant que");
+
+//   if (givenNumber < 22) {
+//     alert("Plus grand");
+//     console.log("plus grand");
+
+//     return false;
+//   } else if (givenNumber > 22) {
+//     alert("Plus petit");
+//     console.log("plus petit");
+
+//     return false;
+//   } else {
+//     alert("Bravo ! Vous avez deviné le nombre");
+//     console.log("gagné");
+
+//     return true;
+//   }
+// }
+
+// function gamePlay() {
+//   console.log("toutenun");
+//   let isWin = false;
+//   do {
+//     let givenNumber = askNumber();
+//     isWin = didIWin(givenNumber);
+//   } while (isWin == false);
+// }
+// gamePlay();
+
+function guessNumberPlayerOne() {
+  console.log("devinez un nombre");
+  let playerOneNumber;
   do {
-    givenNumber = parseInt(prompt("Entrez un nombre"));
-  } while (isNaN(givenNumber));
-  return givenNumber;
+    playerOneNumber = parseInt(
+      prompt("Entrez un nombre compris entre 0 et 50")
+    );
+  } while (
+    playerOneNumber < 0 ||
+    playerOneNumber > 50 ||
+    isNaN(playerOneNumber)
+  );
+  console.log(playerOneNumber);
+  return playerOneNumber;
 }
-//askNumber();
-
-function didIWin(givenNumber) {
-  console.log("fonction didwin");
-
-  //   while (givenNumber !== 22) {
-  console.log("tant que");
-  // givenNumber = parseInt(prompt("Entrez un nombre"));
-
-  if (givenNumber < 22) {
-    alert("Plus grand");
-    console.log("plus grand");
-
-    return false;
-  } else if (givenNumber > 22) {
-    alert("Plus petit");
-    console.log("plus petit");
-
-    return false;
-  } else {
-    alert("Bravo ! Vous avez deviné le nombre");
-    console.log("gagné");
-
-    return true;
-  }
-  //   }
-}
-
-// didIWin();
-
-function gamePlay() {
-  console.log("toutenun");
-  let isWin = false;
-  do {
-    let givenNumber = askNumber();
-    isWin = didIWin(givenNumber);
-  } while (isWin == false);
-}
-
-gamePlay();
+guessNumberPlayerOne();
