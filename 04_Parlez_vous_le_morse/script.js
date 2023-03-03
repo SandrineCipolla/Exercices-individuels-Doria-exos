a47f29203454eaaaade9e10c5a62b2bf978835fb
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-03-03 10:35:16 +0100

    revue etape 1

--- 04_Parlez_vous_le_morse/script.js
+++ 04_Parlez_vous_le_morse/script.js
@@ -1,9 +1,64 @@
-function getLatinCharacterList(array) {
-  for (var i = 0; i < array.length; i++) {
-    console.log(array[i]);
-    document.getElementById("arrays").innerText += "\n" + array[i];
-    // document.getElementById("arrays").innerText += "\n";
-  }
-  document.getElementById("arrays").innerText += "\n------------------\n";
+// function getLatinCharacterList(array) {
+//   for (var i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+//     document.getElementById("arrays").innerText += "\n" + array[i];
+//     // document.getElementById("arrays").innerText += "\n";
+//   }
+//   document.getElementById("arrays").innerText += "\n------------------\n";
+// }
+// getLatinCharacterList("Hello,world");
+
+// let phrase = "Hello,world";
+// let splits = phrase.split(/(\d)/);
+// console.log(splits);
+
+// function getLatinCharacterList(texte) {
+//   //   let phrase = "Hello,world";
+//   for (var i = 0; i < texte.length; i++) {
+//     console.log(texte[i]);
+//     document.getElementById("arrays").innerText += "\n" + texte[i];
+//   }
+// }
+// getLatinCharacterList("Hello,world");
+
+// const chars = str.split('');
+// console.log(chars[8]);
+
+function getLatinCharacterList() {
+  let phrase = "Hello,world";
+  let chars = phrase.split("");
+
+  console.log(chars);
+  document.write(chars);
+  return chars;
 }
-getLatinCharacterList("Hello,world");
+getLatinCharacterList();
+
+const latinToMorse = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
