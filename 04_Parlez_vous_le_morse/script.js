3e479bdd5db9010432fbe641efc9792d8859ad9b
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-03-03 14:48:26 +0100

    etape 2

--- 04_Parlez_vous_le_morse/script.js
+++ 04_Parlez_vous_le_morse/script.js
@@ -25,40 +25,105 @@
 // console.log(chars[8]);
 
 function getLatinCharacterList() {
-  let phrase = "Hello,world";
+  let phrase = "Hello, world";
   let chars = phrase.split("");
 
   console.log(chars);
-  document.write(chars);
+  //   document.write(chars);
+  document.getElementById("Etape1").innerText += "\n" + chars;
   return chars;
 }
 getLatinCharacterList();
 
-const latinToMorse = {
-  A: ".-",
-  B: "-...",
-  C: "-.-.",
-  D: "-..",
-  E: ".",
-  F: "..-.",
-  G: "--.",
-  H: "....",
-  I: "..",
-  J: ".---",
-  K: "-.-",
-  L: ".-..",
-  M: "--",
-  N: "-.",
-  O: "---",
-  P: ".--.",
-  Q: "--.-",
-  R: ".-.",
-  S: "...",
-  T: "-",
-  U: "..-",
-  V: "...-",
-  W: ".--",
-  X: "-..-",
-  Y: "-.--",
-  Z: "--..",
-};
+function translateLatinCharacter(a) {
+  const latinToMorse = {
+    A: ".-",
+    B: "-...",
+    C: "-.-.",
+    D: "-..",
+    E: ".",
+    F: "..-.",
+    G: "--.",
+    H: "....",
+    I: "..",
+    J: ".---",
+    K: "-.-",
+    L: ".-..",
+    M: "--",
+    N: "-.",
+    O: "---",
+    P: ".--.",
+    Q: "--.-",
+    R: ".-.",
+    S: "...",
+    T: "-",
+    U: "..-",
+    V: "...-",
+    W: ".--",
+    X: "-..-",
+    Y: "-.--",
+    Z: "--..",
+  };
+  document.getElementById("Etape2").innerText += "\n" + latinToMorse[a];
+  return latinToMorse[a];
+}
+console.log(translateLatinCharacter("A"));
+
+// // function translateLatinCharacter() {
+// const latinToMorse = new Map([
+//   ["A", ".-"],
+//   ["B", "-..."],
+//   ["C", "-.-."],
+//   ["D", "-.."],
+//   ["E", "."],
+//   ["F", "..-."],
+//   ["G", "--."],
+//   ["H", "...."],
+//   ["I", ".."],
+//   ["J", ".---"],
+//   ["K", "-.-"],
+//   ["L", ".-.."],
+//   ["M", "--"],
+//   ["N", "-."],
+//   ["O", "---"],
+//   ["P", ".--."],
+//   ["Q", "--.-"],
+//   ["R", ".-."],
+//   ["S", "..."],
+//   ["T", "-"],
+//   ["U", "..-"],
+//   ["V", "...-"],
+//   ["W", ".--"],
+//   ["X", "-..-"],
+//   ["Y", "-.--"],
+//   ["Z", "--.."],
+// ]);
+// console.log(latinToMorse.get("A"));
+// for (const [clé, valeur] of latinToMorse) {
+//   console.log(clé + "=" + valeur);
+// }
+
+// document.getElementById("Etape2").innerText +=
+//   "\n" + caract + "=" + latinToMorse2[caract];
+// return caract + "=" + latinToMorse2[caract];
+
+// }
+// translateLatinCharacter();
+
+// var dict = { a: 1, b: 2, c: 3 };
+// var keys = Object.keys(dict);
+// var i;
+// var key;
+// for (i = 0; i < keys.length; i++) {
+//   key = keys[0];
+//   console.log(key + "=" + dict[key]);
+// }
+
+// const map = new Map([
+//   ["A", "un"],
+//   [2, "deux"],
+//   [3, "trois"],
+//   [4, "quatre"],
+// ]);
+// for (const [clé, valeur] of map) {
+//   console.log(clé + " = " + valeur);}
